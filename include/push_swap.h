5c766e0a68a8ded04599fb76fe395d3e85d24d98
Ko-HyeJi <hy2ji.k5@gmail.com>
2022-06-30 19:21:18 +0900

    2022-06-30

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: hyko <hyko@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 17:58:35 by hyko              #+#    #+#             */
-/*   Updated: 2022/06/30 16:47:50 by hyko             ###   ########.fr       */
+/*   Updated: 2022/06/30 19:20:39 by hyko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,60 +38,61 @@ typedef struct s_stack
 }	t_stack;
 
 /* ft_split.c */
-int	word_count(char const *s, char c);
-int	word_len(char const *s, char c);
-char	**ft_split(char const *s, char c);
+int			word_count(char const *s, char c);
+int			word_len(char const *s, char c);
+char		**ft_split(char const *s, char c);
 
 /* operation_push.c */
-void	pa(t_stack *stack_a, t_stack *stack_b);
-void	pb(t_stack *stack_a, t_stack *stack_b);
+void		pa(t_stack *stack_a, t_stack *stack_b);
+void		pb(t_stack *stack_a, t_stack *stack_b);
 
 /* operation_reverse_rotate.c */
-void	rra(t_stack *stack_a);
-void	rrb(t_stack *stack_b);
-void	rrr(t_stack *stack_a, t_stack *stack_b);
+void		rra(t_stack *stack_a);
+void		rrb(t_stack *stack_b);
+void		rrr(t_stack *stack_a, t_stack *stack_b);
 
 /* operation_rotate.c */
-void	ra(t_stack *stack_a);
-void	rb(t_stack *stack_b);
-void	rr(t_stack *stack_a, t_stack *stack_b);
+void		ra(t_stack *stack_a);
+void		rb(t_stack *stack_b);
+void		rr(t_stack *stack_a, t_stack *stack_b);
 
 /* operation_swap.c */
-void	sa(t_stack *stack_a);
-void	sb(t_stack *stack_b);
-void	ss(t_stack *stack_a, t_stack *stack_b);
+void		sa(t_stack *stack_a);
+void		sb(t_stack *stack_b);
+void		ss(t_stack *stack_a, t_stack *stack_b);
 
 /* parsing.c */
-void	is_num(char *str);
-void	check_duplicate_and_indexing(t_stack *stack, int data);
+void		is_num(char *str);
+void		check_duplicate_and_indexing(t_stack *stack, int data);
 
 /* push_swap_utils.c */
-size_t	ft_strlen(const char *s);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t		ft_strlen(const char *s);
+size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
 long long	ft_atol(const char *str);
-void	print_error_msg(char *str);
+void		print_error_msg(char *str);
 
 /* push_swap.c */
-void	is_sorted(t_stack *stack);
-void	parsing(t_stack *stack, char **tmp, int i, int j);
+void		is_sorted(t_stack *stack);
+void		parsing(t_stack *stack, char **tmp, int i, int j);
 
 /* sort_less_than_five.c */
-void	find_num_and_push(t_stack *stack_a, t_stack *stack_b, int num);
-void	sort_three_node(t_stack *stack_a, int size);
-void	less_than_five(t_stack *stack_a, t_stack *stack_b);
+void		find_num_and_push(t_stack *stack_a, t_stack *stack_b, int num);
+void		sort_three_node(t_stack *stack_a, int size);
+void		less_than_five(t_stack *stack_a, t_stack *stack_b);
 
 /* sort.c */
-int		check_direction(t_stack *stack_a, int num, int chunk);
-void	atob_next_step(t_stack *stack_a, t_stack *stack_b, int num, int chunk);
-void	a_to_b(t_stack *stack_a, t_stack *stack_b);
-void	btoa_next_step(t_stack *stack_a, t_stack *stack_b, int num);
-void	b_to_a(t_stack *stack_a, t_stack *stack_b);
+int			check_direction(t_stack *stack_a, int num, int chunk);
+void		atob_next_step(
+				t_stack *stack_a, t_stack *stack_b, int num, int chunk);
+void		a_to_b(t_stack *stack_a, t_stack *stack_b);
+void		btoa_next_step(t_stack *stack_a, t_stack *stack_b, int num);
+void		b_to_a(t_stack *stack_a, t_stack *stack_b);
 
 /* stack.c */
-void	stack_element_init(t_stack *stack);
-t_stack	*stack_init(void);
-void	insert_first_node(t_stack *stack, int data);
-void	push(t_stack *stack, int data);
-t_list	*pop(t_stack *stack);
+void		stack_element_init(t_stack *stack);
+t_stack		*stack_init(void);
+void		insert_first_node(t_stack *stack, int data);
+void		push(t_stack *stack, int data);
+t_list		*pop(t_stack *stack);
 
 #endif
--- include/push_swap_bonus.h
+++ include/push_swap_bonus.h
@@ -6,12 +6,12 @@
 /*   By: hyko <hyko@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 17:58:35 by hyko              #+#    #+#             */
-/*   Updated: 2022/06/30 16:53:25 by hyko             ###   ########.fr       */
+/*   Updated: 2022/06/30 19:19:31 by hyko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#ifndef PUSH_SWAP_BONUS_H
+# define PUSH_SWAP_BONUS_H
 
 # include <stdlib.h>
 # include <stdio.h>
@@ -38,73 +38,74 @@ typedef struct s_stack
 }	t_stack;
 
 /* checker_bonus.c */
-void	exe_command(t_stack *stack_a, t_stack *stack_b, char *cmd);
-void	check_is_sorted(t_stack *stack_a, t_stack *stack_b);
-void	parsing(int argc, char **argv, t_stack *stack, int i);
+void		exe_command(t_stack *stack_a, t_stack *stack_b, char *cmd);
+void		check_is_sorted(t_stack *stack_a, t_stack *stack_b);
+void		parsing(int argc, char **argv, t_stack *stack, int i);
 
 /* ft_split.c */
-int		word_count(char const *s, char c);
-int		word_len(char const *s, char c);
-char	**ft_split(char const *s, char c);
+int			word_count(char const *s, char c);
+int			word_len(char const *s, char c);
+char		**ft_split(char const *s, char c);
 
 /* get_next_line_utils.c */
-size_t	ft_strlen(const char *s);
-char	*ft_strdup(const char *s1);
-char	*ft_substr(char const	*s, unsigned int start, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
+size_t		ft_strlen(const char *s);
+char		*ft_strdup(const char *s1);
+char		*ft_substr(char const	*s, unsigned int start, size_t len);
+char		*ft_strjoin(char const *s1, char const *s2);
 
 /* get_next_line.c */
-char	*get_next_line(int fd);
-int		gnl_find_newline(char *read_line);
-char	*gnl_read_line(int fd);
-char	*gnl_expand_backup(char *backup, int fd);
-char	*gnl_cut_backup(char *backup, char *return_line);
+char		*get_next_line(int fd);
+int			gnl_find_newline(char *read_line);
+char		*gnl_read_line(int fd);
+char		*gnl_expand_backup(char *backup, int fd);
+char		*gnl_cut_backup(char *backup, char *return_line);
 
 /* operation_push.c */
-void	pa(t_stack *stack_a, t_stack *stack_b);
-void	pb(t_stack *stack_a, t_stack *stack_b);
+void		pa(t_stack *stack_a, t_stack *stack_b);
+void		pb(t_stack *stack_a, t_stack *stack_b);
 
 /* operation_reverse_rotate.c */
-void	rra(t_stack *stack_a);
-void	rrb(t_stack *stack_b);
-void	rrr(t_stack *stack_a, t_stack *stack_b);
+void		rra(t_stack *stack_a);
+void		rrb(t_stack *stack_b);
+void		rrr(t_stack *stack_a, t_stack *stack_b);
 
 /* operation_rotate.c */
-void	ra(t_stack *stack_a);
-void	rb(t_stack *stack_b);
-void	rr(t_stack *stack_a, t_stack *stack_b);
+void		ra(t_stack *stack_a);
+void		rb(t_stack *stack_b);
+void		rr(t_stack *stack_a, t_stack *stack_b);
 
 /* operation_swap.c */
-void	sa(t_stack *stack_a);
-void	sb(t_stack *stack_b);
-void	ss(t_stack *stack_a, t_stack *stack_b);
+void		sa(t_stack *stack_a);
+void		sb(t_stack *stack_b);
+void		ss(t_stack *stack_a, t_stack *stack_b);
 
 /* parsing.c */
-void	is_num(char *str);
-void	check_duplicate_and_indexing(t_stack *stack, int data);
+void		is_num(char *str);
+void		check_duplicate_and_indexing(t_stack *stack, int data);
 
 /* push_swap_utils.c */
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
 long long	ft_atol(const char *str);
-void	print_error_msg(char *str);
+void		print_error_msg(char *str);
 
 /* sort_less_than_five.c */
-void	find_num_and_push(t_stack *stack_a, t_stack *stack_b, int num);
-void	sort_three_node(t_stack *stack_a, int size);
-void	less_than_five(t_stack *stack_a, t_stack *stack_b);
+void		find_num_and_push(t_stack *stack_a, t_stack *stack_b, int num);
+void		sort_three_node(t_stack *stack_a, int size);
+void		less_than_five(t_stack *stack_a, t_stack *stack_b);
 
 /* sort.c */
-int		check_direction(t_stack *stack_a, int num, int chunk);
-void	atob_next_step(t_stack *stack_a, t_stack *stack_b, int num, int chunk);
-void	a_to_b(t_stack *stack_a, t_stack *stack_b);
-void	btoa_next_step(t_stack *stack_a, t_stack *stack_b, int num);
-void	b_to_a(t_stack *stack_a, t_stack *stack_b);
+int			check_direction(t_stack *stack_a, int num, int chunk);
+void		atob_next_step(
+				t_stack *stack_a, t_stack *stack_b, int num, int chunk);
+void		a_to_b(t_stack *stack_a, t_stack *stack_b);
+void		btoa_next_step(t_stack *stack_a, t_stack *stack_b, int num);
+void		b_to_a(t_stack *stack_a, t_stack *stack_b);
 
 /* stack.c */
-void	stack_element_init(t_stack *stack);
-t_stack	*stack_init(void);
-void	insert_first_node(t_stack *stack, int data);
-void	push(t_stack *stack, int data);
-t_list	*pop(t_stack *stack);
+void		stack_element_init(t_stack *stack);
+t_stack		*stack_init(void);
+void		insert_first_node(t_stack *stack, int data);
+void		push(t_stack *stack, int data);
+t_list		*pop(t_stack *stack);
 
 #endif
